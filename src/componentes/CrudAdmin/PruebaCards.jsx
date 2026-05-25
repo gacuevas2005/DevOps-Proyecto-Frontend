8a7ce1df9ce5c3780f489de1d4f5dc402be3c190
Gabriel <gabriel.acv0504@gmail.com>
2026-05-25 12:07:27 -0400

    cambio de prueba

--- src/componentes/CrudAdmin/PruebaCards.jsx
+++ src/componentes/CrudAdmin/PruebaCards.jsx
@@ -22,7 +22,7 @@ export const PruebaCards = () => {
         <CardComponent
           title="Revisar Ordenes de despacho 🚚"
           description="Consulta los despachos realizados, modifica los registros de intentos o cierra la orden"
-          buttonText="Consultar"
+          buttonText="cambio de muestra "
           onClick={() => {
             setTablaCompras(false);
             setTablaOrdenes(true);
