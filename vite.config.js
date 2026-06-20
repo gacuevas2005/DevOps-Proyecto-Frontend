09aa7d5f9c8975cd23262ab4304be2aabf58f3b1
Gabriel <gabriel.acv0504@gmail.com>
2026-06-20 14:31:53 -0400

    cambios al vite pruebas 2

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://ab4c3407667b94f96af654877f77605c-2136966934.us-east-1.elb.amazonaws.com', // La URL de tu balanceador
+        // Apunta a tu Load Balancer real
+        target: 'http://ab4c3407667b94f96af654877f77605c-2136966934.us-east-1.elb.amazonaws.com',
         changeOrigin: true,
         secure: false,
+        // ESTA LÍNEA ES CLAVE: No elimines el /api si tu backend espera la ruta completa
+        rewrite: (path) => path 
       }
     }
   }
